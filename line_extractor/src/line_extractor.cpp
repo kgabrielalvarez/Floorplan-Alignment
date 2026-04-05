5a4ac7a8a6c0941e7eba4e8121b4c83f0af98978
kevintumibay <kevintumibay@gmail.com>
2026-04-05 13:14:32 +0000

    line detector is working

--- line_extractor/src/line_extractor.cpp
+++ line_extractor/src/line_extractor.cpp
@@ -14,6 +14,19 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/compressed_image.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/objdetect.hpp"
+#include "vector"
+#include "map"
+
+// Class to store 2D lines
+struct Line2D
+{
+    cv::Point2f p1;
+    cv::Point2f p2;
+};
 
 class LineExtractor : public rclcpp::Node
 {
@@ -25,16 +38,67 @@ public:
     sub_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(
         "/cam0/image_raw/compressed", 10,
         std::bind(&LineExtractor::imageCallback, this, std::placeholders::_1));
+
+    // Initialize LSD
+    lsd_ = cv::createLineSegmentDetector(cv::LSD_REFINE_STD);
   }
 
 private:
   // Initialize subscriber
   rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr sub_;
 
+  // Initialize Line Segment Detector (LSD)
+  cv::Ptr<cv::LineSegmentDetector> lsd_;
+
+  // Initialize map to store detected lines
+  std::map<rclcpp::Time, std::vector<Line2D>> lines_timestamped_;
+
+  // Min length line must have to store it
+  const float min_line_length_ = 30.0f;
+
   // Define callback
   void imageCallback(const sensor_msgs::msg::CompressedImage::SharedPtr msg)
   {
-    RCLCPP_INFO(this->get_logger(), "testing testy test");
+    // Check that we are receiving an image
+    cv::Mat img = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_GRAYSCALE);
+    if (img.empty())
+    {
+      RCLCPP_WARN(this->get_logger(), "Empty image frame");
+      return;
+    }
+    else {
+      RCLCPP_INFO(this->get_logger(), "Image received");
+    }
+
+    // Detect lines
+    std::vector<cv::Vec4f> lines_cv;
+    lsd_->detect(img, lines_cv);
+
+    // Store detected lines
+    std::vector<Line2D> lines;
+    for (const auto &l : lines_cv)
+    {
+      // Get line from lines_cv
+      Line2D line{cv::Point2f(l[0], l[1]), cv::Point2f(l[2], l[3])};
+
+      // Filter out lines that are too short
+      float line_length = cv::norm(line.p2 - line.p1);
+      if (line_length < min_line_length_) continue;
+
+      lines.push_back(line);
+
+      // Add detected lines to image
+      cv::line(img, line.p1, line.p2, cv::Scalar(0, 0, 255), 2);
+    }
+    rclcpp::Time timestamp = msg->header.stamp;
+    lines_timestamped_[timestamp] = lines;
+
+    // Display image with detected lines
+    cv::imshow("Camera Frame", img);
+    cv::waitKey(1);
+
+    // Logging message
+    RCLCPP_INFO(this->get_logger(), "%zu lines detected", lines.size());
   }
 };
 
